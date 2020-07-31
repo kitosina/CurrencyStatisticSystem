99b7431699c6eb472f7bd5971dfc4142bd4378a1
kitosina <kitosina@mail.ru>
2020-07-31 21:55:57 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Status</h1>
+
+[![Build Status](https://travis-ci.com/kitosina/CurrencyStaticSystem.svg?branch=master)](https://travis-ci.com/kitosina/CurrencyStaticSystem)
+
+<h2>Currency Static System</h2>
---
+++ src/main/java/ru/kitosins/sibsutis/currency/CurrencyStaticApplication.java
@@ -0,0 +1,13 @@
+package ru.kitosins.sibsutis.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyStaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyStaticApplication.class, args);
+	}
+
+}
