dcef0904b52d6531754a15a6191fac41883bb18a
kitosina <kitosina@mail.ru>
2020-08-03 04:28:45 +0700

    Update method repository

--- src/main/java/ru/kitosins/sibsutis/currency/repository/CurrencyRepository.java
+++ src/main/java/ru/kitosins/sibsutis/currency/repository/CurrencyRepository.java
@@ -1,9 +1,25 @@
 package ru.kitosins.sibsutis.currency.repository;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import ru.kitosins.sibsutis.currency.entity.Currency;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CurrencyRepository extends CassandraRepository<Currency, Long> {
+
+    @AllowFiltering
+    Currency findByDateAndBasicTitleCurrencyAndQuotedTitleCurrency(Date date, String basicTitleCurrency, String quotedTitleCurrency);
+
+    @Query("select MAX(id) FROM currency ALLOW FILTERING")
+    Long findMaxId();
+
+    //добавить запрос на диапазон дат и валюты.
+
 }
--- src/main/java/ru/kitosins/sibsutis/currency/repository/FactRepository.java
+++ src/main/java/ru/kitosins/sibsutis/currency/repository/FactRepository.java
@@ -1,9 +1,19 @@
 package ru.kitosins.sibsutis.currency.repository;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import ru.kitosins.sibsutis.currency.entity.Currency;
 import ru.kitosins.sibsutis.currency.entity.Fact;
+
+import java.util.List;
 
 @Repository
 public interface FactRepository extends CassandraRepository<Fact, Long> {
+
+    @AllowFiltering
+    List<String> findByNameCurrency(String nameCurrency);
+
+
 }
