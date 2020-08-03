fd5117e7818555f3b83e88e777826e49a3e29cb0
kitosina <kitosina@mail.ru>
2020-08-04 03:30:24 +0700

    Fix method update, create new method repository

--- src/main/java/ru/kitosins/sibsutis/currency/service/CurrencyService.java
+++ src/main/java/ru/kitosins/sibsutis/currency/service/CurrencyService.java
@@ -3,38 +3,21 @@ package ru.kitosins.sibsutis.currency.service;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.kitosins.sibsutis.currency.api.entity.AnswerLatestData;
 import ru.kitosins.sibsutis.currency.entity.Currency;
 import ru.kitosins.sibsutis.currency.repository.CurrencyRepository;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-@Service
 @Slf4j
+@Service
 public class CurrencyService implements CurrencyUpdatable {
 
     private CurrencyRepository currencyRepository;
-
     private RestTemplate restTemplate = new RestTemplate();
-//    private static final String API_1 = "https://api.exchangeratesapi.io/2010-01-01?symbols=RUB&base=USD";
-//    private String testApi = "https://api.exchangeratesapi.io/history?start_at=2015-01-01&end_at=2020-08-02&symbols=USD&base=EUR";
-//    https://api.exchangeratesapi.io/history?start_at=2015-01-01&end_at=2020-08-02&symbols=RUB&base=USD
-//    https://api.exchangeratesapi.io/history?start_at=2015-01-01&end_at=2020-08-02&symbols=RUB&base=EUR
-//    https://api.exchangeratesapi.io/history?start_at=2015-01-01&end_at=2020-08-02&symbols=USD&base=EUR
-
-    //URL которые нужны:
-    /*
-    1)https://api.exchangeratesapi.io/latest?symbols=RUB&base=USD
-    2)https://api.exchangeratesapi.io/latest?symbols=RUB,USD&base=EUR
-     */
-
     private static final String API = "https://api.exchangeratesapi.io/latest";
-//    private static final String API_2 = "https://api.exchangeratesapi.io/latest?symbols=RUB&base=USD";
 
     @Autowired
     public CurrencyService(CurrencyRepository currencyRepository) {
@@ -50,7 +33,7 @@ public class CurrencyService implements CurrencyUpdatable {
     }
 
     @Override
-    public ResponseEntity update(@NonNull String symbols, @NonNull String base) {
+    public Currency update(@NonNull String symbols, @NonNull String base) {
         Long id = currencyRepository.findMaxId()+1;
         Currency currencyLoadDate = new Currency();
         String apiRequest = API.concat("?symbols=").concat(symbols).concat("&base=").concat(base);
@@ -66,33 +49,18 @@ public class CurrencyService implements CurrencyUpdatable {
         currencyLoadDate.setBasicTitleCurrency(base);
         currencyLoadDate.setDate(date);
         currencyLoadDate.setValue(value);
-        if (Objects.isNull(currencyRepository.findByDateAndBasicTitleCurrencyAndQuotedTitleCurrency(date, base, symbols))) {
-            return ResponseEntity.ok(currencyRepository.save(currencyLoadDate));
-        } else return ResponseEntity.noContent().build();
+        return Objects.isNull(currencyRepository.findByDateAndBasicTitleCurrencyAndQuotedTitleCurrency(date, base, symbols))
+                ? currencyRepository.save(currencyLoadDate)
+                : null;
     }
 
-    public void delete(Long id) {
-        currencyRepository.deleteById(id);
+    public List<Currency> findByDateGreaterThanEqualAndDateLessThanEqualAndQuotedTitleCurrencyAndBasicTitleCurrency(
+            String dateAfter, String dateBefore, String quotedTitleCurrency, String basicTitleCurrency) {
+
+        String dateAfterFromTime = dateAfter.concat(" 00:00:00+0000");
+        String dateBeforeFromTime = dateBefore.concat(" 00:00:00+0000");
+        return currencyRepository.findByDateGreaterThanEqualAndDateLessThanEqualAndQuotedTitleCurrencyAndBasicTitleCurrency(
+                dateAfterFromTime, dateBeforeFromTime, quotedTitleCurrency, basicTitleCurrency);
     }
 
-//    public AnswerLatestData get() {
-//        return restTemplate.getForEntity(test, AnswerLatestData.class).getBody();
-//    }
-
-//    public AnswerApi get() {
-//        Long Itearto = 2854L;
-//        AnswerApi api = restTemplate.getForEntity(testApi, AnswerApi.class).getBody();
-//        Set<Date> data = api.getRates().keySet();
-//        for (Date key : data) {
-//            Itearto++;
-//            Currency currency = new Currency();
-//            currency.setId(Itearto);
-//            currency.setDate(key);
-//            currency.setValue(api.getRates().get(key).get("USD"));
-//            currency.setBasicTitleCurrency("EUR");
-//            currency.setQuotedTitleCurrency("USD");
-//            currencyRepository.save(currency);
-//        }
-//        return api;
-//    }
 }
--- src/main/java/ru/kitosins/sibsutis/currency/service/CurrencyUpdatable.java
+++ src/main/java/ru/kitosins/sibsutis/currency/service/CurrencyUpdatable.java
@@ -3,10 +3,12 @@ package ru.kitosins.sibsutis.currency.service;
 import lombok.NonNull;
 import org.apache.tinkerpop.shaded.kryo.NotNull;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import ru.kitosins.sibsutis.currency.entity.Currency;
 
+@Component
 public interface CurrencyUpdatable {
 
-    ResponseEntity update(@NonNull String symbols, @NonNull String base);
+    Currency update(@NonNull String symbols, @NonNull String base);
 
 }
