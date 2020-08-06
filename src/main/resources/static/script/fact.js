63b3ce7c2320b8f8c6e3525eb5c476348c4bf617
kitosina <kitosina@mail.ru>
2020-08-06 19:18:13 +0700

    Implementation interaction backend and frontend

--- src/main/resources/static/script/fact.js
+++ src/main/resources/static/script/fact.js
@@ -1,4 +1,22 @@
 var app = angular.module("FACTS",[]);
+
 app.controller("FACT_CONTROLLER",function($scope, $http){
-    console.log("HELLO");
+
+    $scope.nameCurrency = "";
+    $scope.listFact = [];
+
+    $scope.selectCurrency = function () {
+        console.log($scope.nameCurrency);
+        if ($scope.nameCurrency!=="") {
+            var url = "/fact/" + $scope.nameCurrency;
+            $http({
+                url: url,
+                method: "GET"
+            }).then(function (response) {
+                $scope.listFact = response.data.listFact;
+            })
+
+        } else alert("Please input currency!");
+    }
+
 });
