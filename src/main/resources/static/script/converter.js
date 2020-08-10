c22a24f78e8908f5f3f154c5eec51097137c489d
kitosina <kitosina@mail.ru>
2020-08-11 02:41:22 +0700

    Create coverter page and fix date_static select

--- src/main/resources/static/script/converter.js
+++ src/main/resources/static/script/converter.js
@@ -2,22 +2,45 @@ var app = angular.module("CONVERTER",[]);
 
 app.controller("CONVERTER_CONTROLLER",function($scope, $http){
 
-    $scope.nameCurrency = "";
-    $scope.listFact = [];
-    $scope.url = "/fact/";
+    $scope.coupleCurrency = "";
+    $scope.url = "/currency/converter/";
+    $scope.inputValue = null;
+    $scope.outputValue = null;
+    $scope.nameUserForm = "";
 
-    $scope.selectCurrency = function () {
-        console.log($scope.nameCurrency);
-        if ($scope.nameCurrency!=="") {
-            var urlRequest = $scope.url + $scope.nameCurrency;
+
+    $scope.convertCurrency = function () {
+        console.log($scope.coupleCurrency);
+        console.log($scope.inputValue);
+        if (($scope.coupleCurrency !== "") && ($scope.inputValue !== null)){
+            urlRequest = $scope.url + $scope.coupleCurrency;
             $http({
                 url: urlRequest,
                 method: "GET"
             }).then(function (response) {
-                $scope.listFact = response.data.listFact;
+                $scope.nameUserForm = $scope.coupleCurrency.split("/");
+                $scope.outputValue = $scope.inputValue * response.data + " " + $scope.nameUserForm[1];
             })
+        } else alert("Please input form");
 
-        } else alert("Please input currency!");
+
+
+
+
+        // if ($scope.nameCurrency!=="") {
+        //     var urlRequest = $scope.url + $scope.nameCurrency;
+        //     $http({
+        //         url: urlRequest,
+        //         method: "GET"
+        //     }).then(function (response) {
+        //         $scope.listFact = response.data.listFact;
+        //     })
+        //
+        // } else alert("Please input currency!");
     }
 
 });
+
+// function checkCurrencyEntry(nameCurrency1, nameCurrency2) {
+//     if (nameCurrency1 =)
+// }
