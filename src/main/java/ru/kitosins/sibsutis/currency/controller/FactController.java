73f269d3ae68209da7d75d12d9e5cf8e91650a51
kitosina <kitosina@mail.ru>
2020-08-14 05:43:06 +0700

    Correct FactController and Service

--- src/main/java/ru/kitosins/sibsutis/currency/controller/FactController.java
+++ src/main/java/ru/kitosins/sibsutis/currency/controller/FactController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kitosins.sibsutis.currency.entity.Fact;
-import ru.kitosins.sibsutis.currency.service.FactServiceImpl;
+import ru.kitosins.sibsutis.currency.service.FactService;
 
 import java.util.Objects;
 
@@ -12,28 +12,28 @@ import java.util.Objects;
 @RequestMapping("/fact")
 public class FactController {
 
-    private FactServiceImpl factServiceImpl;
+    private FactService factService;
 
     @Autowired
-    public FactController(FactServiceImpl factServiceImpl) {
-        this.factServiceImpl = factServiceImpl;
+    public FactController(FactService factService) {
+        this.factService = factService;
     }
 
     @PostMapping
     public ResponseEntity save(@RequestBody Fact fact) {
-        return ResponseEntity.ok(factServiceImpl.save(fact));
+        return ResponseEntity.ok(factService.save(fact));
     }
 
     @GetMapping
     public ResponseEntity findAll() {
-        return ResponseEntity.ok(factServiceImpl.findAll());
+        return ResponseEntity.ok(factService.findAll());
     }
 
     @GetMapping("/{nameCurrency}")
     public ResponseEntity findByNameCurrency(@PathVariable String nameCurrency) {
-        return Objects.isNull(factServiceImpl.findByNameCurrency(nameCurrency))
+        return Objects.isNull(factService.findByNameCurrency(nameCurrency))
                 ? ResponseEntity.notFound().build()
-                : ResponseEntity.ok(factServiceImpl.findByNameCurrency(nameCurrency));
+                : ResponseEntity.ok(factService.findByNameCurrency(nameCurrency));
     }
 
 }
--- src/main/java/ru/kitosins/sibsutis/currency/service/FactService.java
+++ src/main/java/ru/kitosins/sibsutis/currency/service/FactService.java
@@ -1,15 +1,33 @@
 package ru.kitosins.sibsutis.currency.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.kitosins.sibsutis.currency.entity.Fact;
+import ru.kitosins.sibsutis.currency.repository.FactRepository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
-public interface FactService {
+@Service
+public class FactService {
 
-    List<Fact> findAll();
+    private FactRepository factRepository;
 
-    Fact save(Fact fact);
+    @Autowired
+    public FactService(FactRepository factRepository) {
+        this.factRepository = factRepository;
+    }
 
-    Fact findByNameCurrency(String nameCurrency);
+    public List<Fact> findAll() {
+        return factRepository.findAll();
+    }
 
+    public Fact save(Fact fact) {
+        return factRepository.save(fact);
+    }
+
+    public Fact findByNameCurrency(String nameCurrency) {
+        return factRepository.findByNameCurrency(nameCurrency);
+    }
 }
