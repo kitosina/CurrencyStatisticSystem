f4d671cc13343776f03315980d53f1d781e19db8
kitosina <kitosina@mail.ru>
2020-08-16 06:58:11 +0700

    Add view admin

--- src/main/java/ru/kitosins/sibsutis/currency/controller/WebController.java
+++ src/main/java/ru/kitosins/sibsutis/currency/controller/WebController.java
@@ -1,14 +1,17 @@
 package ru.kitosins.sibsutis.currency.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "/index";
     }
 
     @GetMapping("/date_statistics")
@@ -39,6 +42,11 @@ public class WebController {
     @GetMapping("/registration")
     public String registration() {
         return "registration";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 
 }
