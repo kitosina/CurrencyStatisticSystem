91c5eb68aaca118c4038db541a828bf0d555854a
kitosina <kitosina@mail.ru>
2020-08-16 06:59:05 +0700

    Implementation method admin controller

---
+++ src/main/java/ru/kitosins/sibsutis/currency/controller/AdminController.java
@@ -0,0 +1,41 @@
+package ru.kitosins.sibsutis.currency.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kitosins.sibsutis.currency.service.CurrencyServiceImpl;
+import ru.kitosins.sibsutis.currency.service.UsersService;
+import ru.kitosins.sibsutis.currency.service.UsersServiceImpl;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/administration")
+public class AdminController {
+
+    private UsersServiceImpl usersService;
+    private CurrencyServiceImpl currencyService;
+
+    @Autowired
+    public AdminController(UsersServiceImpl usersService, CurrencyServiceImpl currencyService) {
+        this.usersService = usersService;
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping
+    public ResponseEntity findAll() {
+        return ResponseEntity.ok(usersService.findAll());
+    }
+
+    @DeleteMapping("/delete/by/{username}")
+    public void deleteByUsername(@PathVariable String username) {
+        usersService.deleteById(username);
+    }
+
+    @DeleteMapping("/clear")
+    public void clear() {
+        currencyService.clear();
+    }
+
+}
