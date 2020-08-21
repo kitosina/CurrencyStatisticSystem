3c410205da8684aa81ebae25ce41405985fa3c79
kitosina <kitosina@mail.ru>
2020-08-21 22:33:27 +0700

    New method for running SQL script on the backend

--- src/main/resources/static/script/fact.js
+++ src/main/resources/static/script/fact.js
@@ -4,21 +4,44 @@ app.controller("FACT_CONTROLLER",function($scope, $http, $filter){
 
     $scope.nameCurrency = "";
     $scope.listFact = [];
-    $scope.url = "/fact/";
+    $scope.url = "/fact";
 
+    $scope.selectAllFact = async function () {
+        var urlRequest = $scope.url;
+        $http({
+            url: urlRequest,
+            method: "GET"
+        }).then(function (response) {
+            if (response.data.length === 0) {
+                loadFacts().then(function () {
+                    selectFactForCurrency()
+                })
+            } else selectFactForCurrency()
+        })
+    }
 
-    $scope.selectCurrency = function () {
-        console.log($scope.nameCurrency);
+    selectFactForCurrency = async function () {
         if ($scope.nameCurrency!=="") {
-            var urlRequest = $scope.url + $scope.nameCurrency;
+            var urlRequest = $scope.url + "/" + $scope.nameCurrency;
             $http({
                 url: urlRequest,
                 method: "GET"
             }).then(function (response) {
-                $scope.listFact = response.data.listFact;
+                $scope.listFact = response.data
             })
 
         } else alert("Please input currency!");
+    }
+
+    loadFacts = async function () {
+            alert("loading facts in DB")
+            var urlRequest = $scope.url + "/" + "load";
+            $http({
+                url: urlRequest,
+                method: "GET",
+            }).then(function (response) {
+                console.log("OK");
+            })
     }
 
 
